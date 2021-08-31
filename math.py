11ad8e90321ecda35111ed94117e17d760812528
vibhav dhaimode <vibhavdhaimode@gmail.com>
2021-08-31 22:16:36 +0530

    Added fn header

--- math.py
+++ math.py
@@ -1,21 +1,15 @@
 #Add implementation
 def add(x,y):
-	return x+y
+	pass
 
 #Subtract implementation
 def subtract(x,y):
-	return x-y
+	pass
 
 #Multiply Implementation
 def multiply(x,y):
-	return x*y
+	pass
 
 #Divide Implementation
 def divide(x,y):
-	if y == 0:
-		return DIVIDE_BY_0_ERROR
-	else:
-		return x/y
-# Square implementation
-def square(x):
-	return x*x
+	pass
